939f68d7a7ef1f742fa7217399c48f77f105bd4f
Anubhav Gupta <38766415+jayhawk24@users.noreply.github.com>
2018-12-21 09:39:37 +0530

    Add files via upload

---
+++ JQUERY Main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 5){
+            $("#flour").css("left","643px").css("top","400px");
+            $("#pappers").css("left","630px").css("top","485px");
+            $("#cheese").css("left","640px").css("top","545px");
+            $("#tomato").css("left","590px").css("top","530px");
+            $("#chili").css("left","585px").css("top","445px");
+            $("#gradient").css("left","635px").css("top","445px");
+            $("#peppermint").css("left","615px").css("top","370px");
+            $("#onion").css("left","650px").css("top","345px");
+            $("#garlic").css("left","600px").css("top","470px");
+        } 
+    }); 
+});
+
+
+
+
+
+
